44099d4ae1c3a65de78c9753853ae674790a7533
Henrik Eiding <henrik.eiding@cognite.com>
2020-04-15 11:55:55 +0200

    Modify example

--- example/handler.py
+++ example/handler.py
@@ -1,7 +1,7 @@
 import time
 
 def handle(data, client):
-    print("Hello anders. Will sleep a bit now...")
+    print("Hello Henrik. Will sleep a bit now...")
     time.sleep(1.0)
     print("Did sleep. Will sleep some more, but first, print another line")
     print("Here it is.")
@@ -9,6 +9,6 @@ def handle(data, client):
     print("Ok, I'm done now. Fetching an asset")
     asset = client.assets.retrieve(external_id = "my_special_asset")
     return {
-        "result": 2.0 * data["value"],
+        "result": 4.0 * data["value"],
         "assetName": asset.name,
     }
